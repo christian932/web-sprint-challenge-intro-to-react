ccbcd05d63eba8f594fb85a62b4c2de38d76a79c
christian932 <christianfarnsworth@gmail.com>
2021-05-30 11:38:43 -0700

    Almost done

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
 // Write your Character component here
+import { CharacterContainer, CharacterCard  } from '../StylesComponents.js';
+
+function Character (props) {
+  const characterData = props.props;
+
+  return (
+    <CharacterContainer>
+      {characterData.map(e => {
+        return (
+          <CharacterCard>
+
+            <p><h1>{e.name}</h1> was {e.gender == 'n/a' ? 'created' : 'born'} on {e.birth_year}.</p>
+            <p>They stand at {e.height}cm and weigh {e.mass}kg</p>
+            <p>They were in the following movies: <br />{e.films.map(e => <div>{e}</div>)}</p>
+          </CharacterCard>
+        );
+      })}
+    </CharacterContainer>
+  );
+}
+
+export default Character;
